0048b6a9732bd12e117d4269d0d1b61695d531de
Timon Schreiber <timon.schreiber@live.de>
2024-05-06 22:32:49 +0200

    Setting and checking generator module now in the main file.

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from ply.lex import lex
 from ply.yacc import yacc
+
 from language.lexer.ite_expr import *
 from language.parser.ite_expr import *
+
+from language.parser.code_generation import set_generator_module, check_generator_module
+from interpreter import all_expr
+
+set_generator_module(all_expr)
+check_generator_module(used_procedures_and_classes)
 
 lexer = lex()
 parser = yacc(start='expression')
 env = {}
-# while True:
-#     i=input("repl > ")
+
 i = '''
-    {
-        x := 4;
-        y := 4;
-        z:=if x > y then
-            if x > y then
-                y := y + 1
+{
+    x := 4;
+    y := 4;
+    z:=if x > y then
+        if x > y then
+            y := y + 1
+    else
+        x*y;
+    z := 2;
+    a := 0;
+    b := 0;
+    loop x do
+        if z >= 0 then
+            loop y do
+                {
+                    z := z - 1;
+                    a := a + 1
+                }
         else
-            x*y;
-        z := 2;
-        a := 0;
-        b := 0;
-        loop x do
-            if z >= 0 then
-                loop y do
-                    {
-                        z := z - 1;
-                        a := a + 1
-                    }
-            else
-                b := b+1;
-        while x > z do
-            x := x - 1
-    }
-    '''
+            b := b+1;
+    while x > z do
+        x := x - 1
+}
+'''
 
 result = parser.parse(input=i, lexer=lexer)
 print(i,"\n\t",result.eval(env))
@@ -53,27 +59,4 @@ note:
     11. local_expr ~
 
 https://en.cppreference.com/w/cpp/language/operator_precedence
-TODO: precedence of IMPL not mentioned in cpp-reference
-'''
-
-'''
-[x] loop expr do expr
-[x] for assign; bool_expr; assign do expr
-[O] [for assign; bool_expr; assign do lock var expr]  <--  optional
-        -> Lock ident In expr
-
-[x] if expr then expr
-[x] if expr then expr else expr
-
-[x] While expr Do expr
-
-[o] Local ident assign expr In expr
-
-class LetExpr()
-def init(self, var, value, body):
-    ...
-
-def eval(env):
-    ?, env = self. -->> Vsiehe skript Seite 50 <<
-
 '''
