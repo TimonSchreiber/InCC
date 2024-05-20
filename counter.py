dbf914063d66804801d683d1576658141d29ccd5
Timon Schreiber <timon.schreiber@live.de>
2024-05-20 11:55:05 +0200

    Example for lambda 'counter' now in separate file and runable.

--- counter.py
+++ counter.py
@@ -9,3 +9,24 @@ counter = '''
 ; a4 := c(2)
 ; a5 := c(3)
 }'''
+
+if __name__ == '__main__':
+    from ply.lex import lex
+    from ply.yacc import yacc
+
+    from language.lexer.lambda_expr import *
+    from language.parser.lambda_expr import *
+    from language.parser.code_generation import set_generator_module, check_generator_module
+
+    from interpreter import lambda_expr
+    from interpreter.enviroment import Enviroment
+
+    set_generator_module(lambda_expr)
+    check_generator_module(used_procedures_and_classes)
+
+    lexer = lex()
+    parser = yacc(start='expression')
+    env = Enviroment()
+
+    result = parser.parse(input=counter, lexer=lexer)
+    print(counter, "\n", result.eval(env))
