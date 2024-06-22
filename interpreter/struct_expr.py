eb688e82139eecf0ad5d621b9d55c1b3e653d796
TimonSchreiber <timon.schreiber@live.de>
2024-06-22 18:05:41 +0200

    Swapped class definition order.

--- interpreter/struct_expr.py
+++ interpreter/struct_expr.py
@@ -12,21 +12,6 @@ class StructExpression(InterpretedExpression):
             dict.__setitem__(env1, id, v)  # add value to this level of dict
         return (env1.get_vals(), env)  # return the enviroment WITHOUT parent
 
-class StructMemberAccessExpression(InterpretedExpression):
-    def __init__(self, s, dots, id):
-        self.s = s
-        self.n_dots = len(dots)
-        self.id = id
-
-    def eval(self, env):
-        env1 = env[self.s]
-        for _ in range(self.n_dots-1):
-            if env1.get_parent() is None:
-                raise Exception(f'Not enough inheritance layers: {self.n_dots}')
-            env1 = env1.get_parent()
-        m = env1[self.id]
-        return (m, env)
-
 class StructExtendExpression(InterpretedExpression):
     def __init__(self, s, body):
         self.s = s
@@ -39,3 +24,18 @@ class StructExtendExpression(InterpretedExpression):
             v, env1 = e.eval(env1)
             dict.__setitem__(env1, id, v)  # add value to this level of dict
         return (env1, env)  # return the enviroment WITH parent
+
+class StructMemberAccessExpression(InterpretedExpression):
+    def __init__(self, s, dots, id):
+        self.s = s
+        self.n_dots = len(dots) - 1
+        self.id = id
+
+    def eval(self, env):
+        env1 = env[self.s]
+        for i in range(self.n_dots):
+            if env1.get_parent() is None:
+                raise Exception(f'Inheritance hirachy has not enough layers:\n\t-> Upper limit at {i}, trying to access {self.n_dots}')
+            env1 = env1.get_parent()
+        m = env1[self.id]
+        return (m, env)
